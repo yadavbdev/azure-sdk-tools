f64bbd65eed39df9ad3887674acb85c15c74a20a
Yitao Zhang <yitao.zhang@outlook.com>
2014-08-26 23:44:07 -0700

    Added DataFactory cmdlets and test project.

---
+++ src/ResourceManager/DataFactories/Commands.DataFactories/DataFactories/GetAzureDataFactoryCommand.cs
@@ -0,0 +1,53 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Security.Permissions;
+
+using Microsoft.Azure.Commands.DataFactories.Models;
+
+namespace Microsoft.Azure.Commands.DataFactories
+{
+    [Cmdlet(VerbsCommon.Get, Constants.DataFactory), OutputType(typeof(List<PSDataFactory>), typeof(PSDataFactory))]
+    public class GetAzureDataFactoryCommand : DataFactoryBaseCmdlet
+    {
+        [Parameter(Position = 1, Mandatory = false, ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The data factory name.")]
+        public string Name { get; set; }
+        
+        [EnvironmentPermission(SecurityAction.Demand, Unrestricted = true)]
+        public override void ExecuteCmdlet()
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(Name))
+                {
+                    var dataFactories = DataFactoryClient.ListDataFactories(ResourceGroupName);
+                    WriteObject(dataFactories, true);
+                }
+                else
+                {
+                    PSDataFactory dataFactory = DataFactoryClient.GetDataFactory(ResourceGroupName, Name);
+                    WriteObject(dataFactory);    
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteExceptionError(ex);
+            }
+        }
+    }
+}
---
+++ src/ResourceManager/DataFactories/Commands.DataFactories/Models/DataFactoryClient.cs
@@ -0,0 +1,60 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+using Microsoft.Azure.Commands.DataFactories.Models;
+using Microsoft.Azure.Management.DataFactories;
+using Microsoft.Azure.Management.DataFactories.Models;
+using Microsoft.WindowsAzure.Commands.Utilities.Common;
+
+namespace Microsoft.Azure.Commands.DataFactories
+{
+    public class DataFactoryClient : IDataFactoryClient
+    {
+        public IDataPipelineManagementClient DataPipelineManagementClient { get; internal set; }
+
+        public Action<string> Logger { get; set; }
+
+        public DataFactoryClient(WindowsAzureSubscription subscription, Action<string> logger)
+        {
+            Logger = logger;
+            DataPipelineManagementClient =
+                subscription.CreateClientFromResourceManagerEndpoint<DataPipelineManagementClient>();
+        }
+
+        public PSDataFactory GetDataFactory(string resourceGroupName, string dataFactoryName)
+        {
+            var response = DataPipelineManagementClient.DataFactories.Get(resourceGroupName, dataFactoryName);
+
+            return new PSDataFactory(response.DataFactory) { ResourceGroupName = resourceGroupName };
+        }
+
+        public List<PSDataFactory> ListDataFactories(string resourceGroupName)
+        {
+            List<PSDataFactory> dataFactories = new List<PSDataFactory>();
+
+            var response = DataPipelineManagementClient.DataFactories.List(resourceGroupName);
+
+            if (response != null && response.DataFactories != null)
+            {
+                response.DataFactories.ForEach(
+                    df => dataFactories.Add(new PSDataFactory(df) { ResourceGroupName = resourceGroupName }));
+            }
+
+            return dataFactories;
+        }
+    }
+}
