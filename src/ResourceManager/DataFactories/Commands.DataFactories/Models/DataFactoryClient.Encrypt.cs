5b9475dc0c69a3e8e14578bd0a2fe01237628872
Bing Li <bingl@microsoft.com>
2014-10-05 20:56:24 +0800

    Add New-AzureDataFactoryEncryptValue cmdlet a) This cmdlet is used for encrypting a connection string for azure data factory on-premises gateway scenario,     and for encrypting a sensitive string for azure data factory cloud scenario. This change set only contains the on-premises     encryption scenario. The clould scenario will be added in the other PR by the other developer. b) Add related unit test case.

---
+++ src/ResourceManager/DataFactories/Commands.DataFactories/Models/DataFactoryClient.Encrypt.cs
@@ -0,0 +1,56 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+using System.Linq;
+using Microsoft.Azure.Commands.DataFactories.Models;
+using Microsoft.Azure.Commands.DataFactories.Properties;
+using Microsoft.Azure.Management.DataFactories.Models;
+using Microsoft.Azure.Management.DataFactories;
+using Microsoft.DataTransfer.Gateway.Encryption;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Commands.Utilities.Common;
+using Microsoft.WindowsAzure.Commands.Utilities;
+using System.Security;
+
+namespace Microsoft.Azure.Commands.DataFactories
+{
+    public partial class DataFactoryClient
+    {
+        public virtual string EncryptString(SecureString value, string resourceGroupName, string dataFactoryName, string gatewayName)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+
+            var response = DataPipelineManagementClient.Gateways.RetrieveConnectionInfo(resourceGroupName, dataFactoryName, gatewayName);
+            var gatewayEncryptionInfos = new[]
+                {
+                    new GatewayEncryptionInfo
+                        {
+                            ServiceToken = response.ConnectionInfo.ServiceToken,
+                            IdentityCertThumbprint = response.ConnectionInfo.IdentityCertThumbprint,
+                            HostServiceUri = response.ConnectionInfo.HostServiceUri
+                        }
+                };
+
+            var gatewayEncryptionClient = new GatewayEncryptionClient();
+            return gatewayEncryptionClient.Encrypt(value, gatewayEncryptionInfos);
+        }
+    }
+}
